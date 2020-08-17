fa692ae5ff6253d102b451f697d4f6b3a8a97f33
Vinicius Pereira de Oliveira <agavi2014@hotmail.com>
2020-08-17 11:42:38 -0300

    first commit of second plugin

---
+++ update_main_page_admin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Alteração da pagina admin
+Plugin URI:
+Description: Primeiro plugin criado para realizar alterações no admin
+Version: 1.0
+Author: Vinícius Pereira de Oliveira
+Author URI:
+Text Domain:
+License:
+*/
