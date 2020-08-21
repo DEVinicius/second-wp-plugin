178f7f7a6b7adfd4304653ef5bc378039088f76b
Vinicius Pereira de Oliveira <agavi2014@hotmail.com>
2020-08-20 21:37:58 -0300

    adicionando css ao plugin

--- update_main_page_admin.php
+++ update_main_page_admin.php
@@ -28,17 +28,23 @@ class SecondPlugin
     private function __construct()
     {
         remove_action('welcome_panel', 'wp_welcome_panel');
+        add_action('welcome_panel', array($this,'updateMainPage'));
+        add_action('admin_enqueue_scripts', array($this,'addCss'));
+    }
 
-        function updateMainPage()
-        {
-            ?>
-                <div class="welcome-panel-content">
-                    <h3>Vinicius Pereira de oLiveira</h3>
-                </div>
-            <?php 
-        }
-        
-        add_action('welcome_panel', 'updateMainPage');
+    public function updateMainPage()
+    {
+        ?>
+            <div class="welcome-panel-content">
+                <h3>Vinicius Pereira de oLiveira</h3>
+            </div>
+        <?php 
+    }
+
+    public function addCss()
+    {
+        wp_register_style('mySecondPlugin', plugin_dir_url(__FILE__)."css/style.css");
+        wp_enqueue_style('mySecondPlugin');
     }
 }
 
